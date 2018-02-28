bf0c79ac79cbe506f75057d025065acf84bef8e4
Sierra McCahon <smitche4@mail.umw.edu>
2018-02-28 22:16:39 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -6,13 +6,4 @@ def speak_to_grandma(phrase)
   		return "HUH?! SPEAK UP, SONNY!"
     elsif phrase == phrase.upcase
   		return "NO, NOT SINCE 1938!"
-=======
-    if phrase == "I love you grandma!".upcase
-      puts "I LOVE YOU TOO PUMPKIN!"
-    elsif phrase == phrase.downcase
-  		puts "HUH?! SPEAK UP, SONNY!"
-    else phrase == phrase.upcase
-  		puts "NO, NOT SINCE 1938!"
->>>>>>> 36b006f539cd7114bc0e7723a7e56af3e2182101
-    end
-end
+  		EN
